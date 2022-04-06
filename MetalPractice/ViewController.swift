f69710acadd35455c7b7babedf3bd8efb761abcd
郭毅 <13311300131@189.cn>
2022-04-06 15:49:44 +0800

    Add files via upload

---
+++ MetalPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetalPractice
+//
+//  Created by 郭毅 on 2022/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
